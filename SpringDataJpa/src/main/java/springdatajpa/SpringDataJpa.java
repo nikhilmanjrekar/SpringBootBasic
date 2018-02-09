59da9058765c49fb1d8f6277d0a0d7c510dcafd8
Nikhil Manjrekar <36074077+nikhilmanjrekar@users.noreply.github.com>
2018-02-09 15:14:46 +0530

    Add files via upload

---
+++ SpringDataJpa/src/main/java/springdatajpa/SpringDataJpa.java
@@ -0,0 +1,10 @@
+package springdatajpa;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpa {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpa.class, args);
+	}
+}
